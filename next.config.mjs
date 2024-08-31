e69ad9585a300da424f1b47f1d48c3f1d247fe9e
sadeeqdev <boskitechh@gmail.com>
2024-08-31 16:32:09 +0100

    feat: dokerize whole app

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
+  output: "standalone", // Outputs a Single-Page Application (SPA).
   antropicKey: process.env.NEXT_PUBLIC_ANTHROPIC_KEY,
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 "use client";
-import React from "react";
 import WelcomePage from "../components/WelcomePage";
 import "../index.css";
 
