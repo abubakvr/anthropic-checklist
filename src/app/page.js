4a4fcafb35474e754c21b21b6ef8bef8b602829b
sadeeqdev <boskitechh@gmail.com>
2024-09-12 23:49:07 +0100

    fix: initiate react-ga

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,10 @@
 import WelcomePage from "../components/WelcomePage";
 import "../index.css";
 import ReactGA from "react-ga";
-import { useEffect } from "react";
 
 const GA_TRACKING_ID = process.env.ANALYTICS_ID;
 const Page = () => {
-  useEffect(() => {
-    ReactGA.initialize(GA_TRACKING_ID);
-  }, []);
+  ReactGA.initialize(GA_TRACKING_ID);
   return <WelcomePage>page</WelcomePage>;
 };
 
