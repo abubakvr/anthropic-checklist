efeea36d72a2eae448f85e996446ba6e51c8b156
sadeeqdev <boskitechh@gmail.com>
2024-09-12 23:26:06 +0100

    fix: use google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 export const metadata = {
   title: "My App",
   description: "My App is a checklist app",
@@ -8,27 +10,10 @@ const GA_TRACKING_ID = process.env.ANALYTICS_ID;
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', ${GA_TRACKING_ID}, {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-      </head>
       <body suppressHydrationWarning={true}>
         <div id="root">{children}</div>
       </body>
+      <GoogleAnalytics gaId={GA_TRACKING_ID} />
     </html>
   );
 }
