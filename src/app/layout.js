a52d1f09c2f2446f509b997f4015f5b1baefaf0c
sadeeqdev <boskitechh@gmail.com>
2024-09-12 22:43:39 +0100

    fix: include script tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${GA_TRACKING_ID}, {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </head>
       <body suppressHydrationWarning={true}>
         <div id="root">{children}</div>
       </body>
